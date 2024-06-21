d16801cc1e3e2ca58489419789f807408589fc89
Aayush Kumar <aayush.daftuar@gmail.com>
2024-06-21 09:07:43 -0700

    added stars

---
+++ stars.py
@@ -0,0 +1,18 @@
+#example 1
+
+for i in range(5):
+    print('*' * 5)
+
+#example 2
+
+for i in range(1, 6):
+    print('*' * i)
+
+#example 3
+    
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end='')
+    print()
+
+    
