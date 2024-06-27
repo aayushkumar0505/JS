b668e1b6528618e49ecac4e86dacf432d4e8b0d0
Aayush Kumar <aayush.daftuar@gmail.com>
2024-06-26 23:51:45 -0700

    added files

---
+++ binarysearch.py
@@ -0,0 +1,21 @@
+#Binary search has a runtime complexity of O(log n)
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
+
+arr = [2, 3, 4, 10, 40]
+target = 10
+
+result = binary_search(arr, target)
+
+assert(result == 3)
---
+++ runtime.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3, 4, 5]
+
+# For loop with O(n) time complexity
+for element in my_list:
+    print(element)
